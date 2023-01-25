98d38b323830731dd67a8fe232f31cf4f239968a
Suyi <thonatos.yang@gmail.com>
2023-01-26 01:26:27 +0800

    feat: add telegram volunteer support

--- src/constants.ts
+++ src/constants.ts
@@ -2,6 +2,7 @@ export const TELEGRAM_SERVICE_IDS = [
   '777000', // Telegram
   '609517172', // Telegram Bot - Rose
   '178220800', // Telegram Bot - Spambot
+  '424000', // Telegram Support - Volunteer
 ];
 
 export const DEFAULT_DATA = {
